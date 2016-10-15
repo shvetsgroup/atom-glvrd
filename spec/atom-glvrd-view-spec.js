c1ba9b39430b5c1f32466b5c520fa78e69d44870
Alexander Shvets <neochief@shvetsgroup.com>
2016-10-15 12:15:56 +0300

    Initial commit.

---
+++ spec/atom-glvrd-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomGlvrdView from '../lib/atom-glvrd-view';
+
+describe('AtomGlvrdView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
