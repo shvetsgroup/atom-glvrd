305e9c4927cbeae0b8517e42a3818a7c88d99dce
Alexander Shvets <neochief@shvetsgroup.com>
2016-10-20 18:53:24 +0300

    Update.

--- spec/atom-glvrd-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import AtomGlvrdView from '../lib/atom-glvrd-view';
-
-describe('AtomGlvrdView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
