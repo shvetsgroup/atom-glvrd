388a899f0268dc92aece0b3a052d72770596fcbb
Alexander Shvets <neochief@shvetsgroup.com>
2016-10-28 14:14:54 +0300

    Update.

---
+++ spec/glavred-spec.js
@@ -0,0 +1,29 @@
+'use babel';
+
+import AtomGlvrdView from '../lib/atom-glvrd-view';
+
+describe("When I run glavred", () => {
+    var workspaceElement, editor, editorElement;
+
+    beforeEach(() => {
+        workspaceElement = atom.views.getView(atom.workspace);
+
+        waitsForPromise(() => {
+            return atom.workspace.open('test.md');
+        });
+        waitsForPromise(() => {
+            return atom.packages.activatePackage('atom-glvrd');
+        });
+        runs(() => {
+            editor = atom.workspace.getActiveTextEditor();
+            editorElement = atom.views.getView(editor);
+        });
+    });
+    it("checks and highlights text", () => {
+        editor.setText("Суть жизни неизвестна.");
+        atom.commands.dispatch(editorElement, 'atom-glvrd:toggle');
+
+        var panels = atom.workspace.getRightPanels();
+        expect(panels.length).toBe(1);
+    })
+});
